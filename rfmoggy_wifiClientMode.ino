3f2f6067df581e6a4fb9b81e6ac4df8ed880ed7b
GusGorman <gusgorman402@gmail.com>
2020-05-20 14:01:34 -0500

    Update rfmoggy_wifiClientMode.ino

--- rfmoggy_wifiClientMode.ino
+++ rfmoggy_wifiClientMode.ino
@@ -19,7 +19,6 @@ byte sendPacket[60];
 String debruijn_ten = "0000000000100000000110000000101000000011100000010010000001011000000110100000011110000010001000001001100000101010000010111000001100100000110110000011101000001111100001000010001100001001010000100111000010100100001010110000101101000010111100001100010000110011000011010100001101110000111001000011101100001111010000111111000100010100010001110001001001000100101100010011010001001111000101001100010101010001010111000101100100010110110001011101000101111100011000110010100011001110001101001000110101100011011010001101111000111001100011101010001110111000111100100011110110001111101000111111100100100110010010101001001011100100110110010011101001001111100101001010011100101010110010101101001010111100101100110010110101001011011100101110110010111101001011111100110011010011001111001101010100110101110011011011001101110100110111110011100111010110011101101001110111100111101010011110111001111101100111111010011111111010101010111010101101101010111110101101011011110101110111010111101101011111110110110111011011111101110111110111101111111111000000000";
 String debruijn_nine = "0000000001000000011000000101000000111000001001000001011000001101000001111000010001000010011000010101000010111000011001000011011000011101000011111000100011000100101000100111000101001000101011000101101000101111000110011000110101000110111000111001000111011000111101000111111001001001011001001101001001111001010011001010101001010111001011011001011101001011111001100111001101011001101101001101111001110101001110111001111011001111101001111111010101011010101111010110111010111011010111111011011011111011101111011111111100000000";
 String debruijn_eight = "00000000100000011000001010000011100001001000010110000110100001111000100010011000101010001011100011001000110110001110100011111001001010010011100101011001011010010111100110011010100110111001110110011110100111111010101011101011011010111110110111101110111111110000000";
-String jam_packet = "111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111";
 
 
 String jukebox_pwm_prefix = "11111111111111110000000010100010100010001000101000";  //preamble sync and vendor ID x5D converted to PWM. NEC Infrared Protocol
@@ -36,7 +35,7 @@ String jukebox_menu = "";
 String garage_menu = "";
 String debruijn_menu = "";
 String rfpwnon_menu = "";
-String jammer_menu = "";
+
 
 void handle_NotFound(){
   server.send(404, "text/plain", "Not found");
@@ -66,11 +65,6 @@ void handleDebruijnMenu(){
 void handleRfpwnonMenu(){
   Serial.println("User called the Rfpwnon menu");
   server.send(200, "text/html", rfpwnon_menu);
-}
-
-void handleJammerMenu(){
-  Serial.println("User called the Jammer menu");
-  server.send(200, "text/html", jammer_menu);
 }
 
 
@@ -188,18 +182,7 @@ void handleRfpwnonSend(){
   server.send(200, "text/html", menu_htmlHeader + "<h1>Signal is sent</h1><p><a href=\"/\">Return to Main Menu</a></p><p><a href=\"rfpwnon\">Return to RfPwnOn Menu</a></p>");
 }
 
-void handleJammerSend(){
-  float jamMHz = server.arg("jammerMHz").toFloat();
-  int jamTime = server.arg("jammerTime").toInt();
-  //at 2kbps we can send about 250bytes per second. 50bytes per packet = 5 packets (repeats) per second
-  int repeatJam = jamTime * 5;
 
-  Serial.println(); Serial.println("TX: Jammer Signal"); Serial.println("----------");
-  binaryBruteSend(0, "0", "1", jamMHz, 2.0, repeatJam, 1, 1,  "", false, jam_packet, false);  
-  Serial.println("----------"); Serial.println("EOTX: Jammer Signal"); Serial.println();
-
-  server.send(200, "text/html", menu_htmlHeader + "<h1>Signal is sent</h1><p><a href=\"/\">Return to Main Menu</a></p><p><a href=\"jammer\">Return to Jammer Menu</a></p>");
-}
 
 //input dip switch size, PWM code for "0" and "1", frequency, datarate, repeatTimes, gap between repeats milliseconds 
 void binaryBruteSend(int dipSize, String zero_pwm, String one_pwm, float freqmhz, float baud, int pulseReps, int gapWidth, 
@@ -404,17 +387,6 @@ void setup(void){
   rfpwnon_menu += "<p><input type=\"submit\" value=\"Submit\"></p>";
   rfpwnon_menu += "</form><p><a href=\"/\">Return to Main Menu</a></p></body></html>";
 
-  jammer_menu += menu_htmlHeader;
-  jammer_menu += "<title>Jammer</title>";
-  jammer_menu += "<body><h1>Jammer</h1>";
-  jammer_menu += "<h3>Simple Jammer - ASK Modulated</h3>";
-  jammer_menu += "<p>Creates continuous data packets(50bytes) full of \"1\", \"ON\", 0xFF bytes at 2kbps</p>";
-  jammer_menu += "<form action=\"sendjammer\">";
-  jammer_menu += "<p><label>Jammer Freq MHz </label><input type=\"text\" name=\"jammerMHz\" size=\"10\" value=\"859.7875\"></p>";
-  jammer_menu += "<p><label>Seconds to Jam </label><input type=\"text\" name=\"jammerTime\" size=\"10\" value=\"30\"></p>";
-  jammer_menu += "<p><input type=\"submit\" value=\"Submit\"></p>";
-  jammer_menu += "</form><p><a href=\"/\">Return to Main Menu</a></p></body></html>";
-
   
   
     
@@ -459,12 +431,12 @@ void setup(void){
   server.on("/garage", handleGarageMenu);
   server.on("/debruijn", handleDebruijnMenu);
   server.on("/rfpwnon", handleRfpwnonMenu);
-  server.on("/jammer", handleJammerMenu);
+  
   server.on("/sendjukebox", handleJukeboxSend);
   server.on("/sendgarage", handleGarageSend);
   server.on("/senddebruijn", handleDebruijnSend);
   server.on("/sendrfpwnon", handleRfpwnonSend);
-  server.on("/sendjammer", handleJammerSend);
+  
   server.onNotFound(handle_NotFound);
  
   server.begin();                  //Start server
